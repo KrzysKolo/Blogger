61fad6851e82b20014d48ba6177f4ca6b69d3ba3
Krzysztofa Kołodziejczak <krzysztofa.kolodziejczak@gmail.com>
2021-03-25 12:45:46 +0100

    Dodaj pliki projektów.

---
+++ Domain/Common/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public DataSetDateTime? LastModified { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
